5b5c8dabc83f43ee249f6be1ad8949f1be38bd75
joannenguyen <nguyenjoanne23@gmail.com>
2015-10-12 07:46:23 -0400

    needs tweaking but about 90% done

--- js/bigslide.js
+++ js/bigslide.js
@@ -12,19 +12,19 @@
 /******Big Hero Slider*******/
 
 
-$('.cd-slider-nav li').on('click', function(event){
-	event.preventDefault();
-	var selectedItem = $(this);
-	if(!selectedItem.hasClass('selected')) {
-		// if it's not already selected
-		var selectedPosition = selectedItem.index(),
-			activePosition = $('.cd-hero-slider .selected').index();
-		if( activePosition < selectedPosition) {
-			nextSlide($('.cd-hero-slider'), $('.cd-slider-nav'), selectedPosition);
-		} else {
-			prevSlide($('.cd-hero-slider'), $('.cd-slider-nav'), selectedPosition);
-		}
+// $('.cd-slider-nav li').on('click', function(event){
+// 	event.preventDefault();
+// 	var selectedItem = $(this);
+// 	if(!selectedItem.hasClass('selected')) {
+// 		// if it's not already selected
+// 		var selectedPosition = selectedItem.index(),
+// 			activePosition = $('.cd-hero-slider .selected').index();
+// 		if( activePosition < selectedPosition) {
+// 			nextSlide($('.cd-hero-slider'), $('.cd-slider-nav'), selectedPosition);
+// 		} else {
+// 			prevSlide($('.cd-hero-slider'), $('.cd-slider-nav'), selectedPosition);
+// 		}
 		
-		updateNavigationMarker(selectedPosition+1);
-	}
-});
+// 		updateNavigationMarker(selectedPosition+1);
+// 	}
+// });
